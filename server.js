aada3ed0cf0f12d6adf978a059ab5d7c92c92d7b
chaitanyakapre <chaitanyakapre%@@gmail.com>
2023-06-19 09:54:32 +0530

    bug fixed

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const { POSTS } = require('./posts');
 const express = require('express');
 const app = express();
 const port = 8080;
 
-
+const { POSTS } = require('./posts');
+const posts = POSTS;
 module.exports = app.get('/feed', (req, res) => {
   const searchQuery = req.query.search || '';
   const sortBy = req.query.sortBy || 'name';
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
-
+  
   // Search
   const searchResults = searchPosts(searchQuery);
   // Sort
@@ -30,13 +30,12 @@ module.exports = app.get('/feed', (req, res) => {
 function searchPosts(query) {
   if (!query) {
     // Return all posts if no query is provided
-    return POSTS;
+    return posts;
   }
 
   const isExactMatch = query.startsWith('"') && query.endsWith('"');
   const normalizedQuery = isExactMatch ? query.slice(1, -1) : query.toLowerCase();
-
-  return POSTS.filter((post) => {
+  return posts.filter((post) => {
     const normalizedPostName = post.name.toLowerCase();
     const normalizedPostDescription = post.description.toLowerCase();
     // isExactMatch represents the case when search term is in " "
@@ -55,21 +54,21 @@ function searchPosts(query) {
   });
 }
 
-function sortPosts(POSTS, sortBy) {
+function sortPosts(posts, sortBy) {
   if (sortBy === "name") {
-    return POSTS.sort((a, b) => a.name.localeCompare(b.name));
+    return posts.sort((a, b) => a.name.localeCompare(b.name));
   } else if (sortBy === "dateLastEdited") {
-    return POSTS.sort((a, b) => Date.parse(a?.dateLastEdited) - Date.parse(b?.dateLastEdited));
+    return posts.sort((a, b) => Date.parse(a?.dateLastEdited) - Date.parse(b?.dateLastEdited));
   }
 
   // If sortBy is not specified or invalid, original posts are returned
-  return POSTS;
+  return posts;
 }
 
-function getPaginatedResult(POSTS, page, pageSize) {
+function getPaginatedResult(posts, page, pageSize) {
   const startIndex = (page - 1) * pageSize;
   const endIndex = startIndex + pageSize;
-  const paginatedPosts = POSTS.slice(startIndex, endIndex);
+  const paginatedPosts = posts.slice(startIndex, endIndex);
 
   return {
     data: paginatedPosts,
